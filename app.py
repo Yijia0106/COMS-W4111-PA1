581a41d897a7043d2e03e34743c475dc3b504123
burushuijiaoba <ryanyijia@gmail.com>
2022-11-09 18:01:04 -0500

    changes

--- app.py
+++ app.py
@@ -100,5 +100,31 @@ def teardown_request(exception):
         pass
 
 
+#
+# if __name__ == "__main__":
+#     app.run(debug=True, port=8111)
+
 if __name__ == "__main__":
-    app.run(debug=True, port=8111)
+    import click
+
+
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8111, type=int)
+    def run(debug, threaded, host, port):
+        """
+    This function handles command line parameters.
+    Run the server using
+        python server.py
+    Show the help text using
+        python server.py --help
+    """
+
+        HOST, PORT = host, port
+        print("running on %s:%d" % (HOST, PORT))
+        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+
+    run()
