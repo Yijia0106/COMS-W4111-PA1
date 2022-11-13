7fd2c7af0ef010a8965e8c638f449adf1757a4c9
YaochenS <ycshen@live.unc.edu>
2022-11-12 21:17:48 -0500

    first finished draft

--- app.py
+++ app.py
@@ -30,12 +30,14 @@ def index():
 @app.route('/submitAvailabilityRequestByName', methods=['POST'])
 def submitAvailabilityRequestByName():
     name = request.form.get('byName')
+    # cursor = g.conn.execute(
+    #     "SELECT name, row, seat, section, theatre_name as Theatre, cdate as Date, ctime as Time, price, temp1.location_id, temp2.sid, temp1.ccid FROM (AvailableOn a inner join (Select cid as ccid, cdate, ctime From Calendars) cal ON a.cid = cal.ccid) temp1 inner join (Select location_id, theatre_name, l.row_id as row, l.seat_id as seat, l.section, name, s.sid From Locations l Left join (SELECT * FROM shows WHERE name ='{name1}') s on l.sid = s.sid) temp2 ON temp1.location_id = temp2.location_id WHERE order_number is Null and temp2.name ='{name1}'".format(
+    #         name1=name))
     cursor = g.conn.execute(
-        "SELECT name, row, seat, section, theatre_name as Theatre, cdate as Date, ctime as Time, price, temp1.location_id, temp2.sid, temp1.ccid FROM (AvailableOn a inner join (Select cid as ccid, cdate, ctime From Calendars) cal ON a.cid = cal.ccid) temp1 inner join (Select location_id, theatre_name, l.row_id as row, l.seat_id as seat, l.section, name, s.sid From Locations l Left join (SELECT * FROM shows WHERE name ='{name1}') s on l.sid = s.sid) temp2 ON temp1.location_id = temp2.location_id WHERE order_number is Null and temp2.name ='{name1}'".format(
-            name1=name))
+        "SELECT name, row, seat, section, theatre_name as Theatre, cdate as Date, ctime as Time, price, temp1.location_id, temp2.sid, temp1.ccid FROM (AvailableOn a inner join (Select cid as ccid, cdate, ctime From Calendars) cal ON a.cid = cal.ccid) temp1 inner join (Select location_id, theatre_name, l.row_id as row, l.seat_id as seat, l.section, name, s.sid From Locations l Left join (SELECT * FROM shows WHERE name =%s) s on l.sid = s.sid) temp2 ON temp1.location_id = temp2.location_id WHERE order_number is Null and temp2.name =%s", name, name)
     shows = []
     for entry in cursor:
-        print(entry)
+        # print(entry)
 
         show = dict()
         show['name'] = entry[0]
@@ -64,10 +66,10 @@ def submitAvailabilityRequestByDate():
         to_date = request.form.get('toDate')
         shows = []
         cursor = g.conn.execute(
-            "Select name as Show, row, seat, section, theatre_name as Theatre, cdate as Date, ctime as Time, price,  temp1.location_id, temp2.sid, temp1.ccid From (AvailableOn a inner join (Select cid as ccid, cdate, ctime From Calendars c Where c.cdate between CAST('{from_date}' AS DATE) and CAST('{to_date}' AS DATE)) cal on a.cid = cal.ccid) temp1 Left join (Select location_id, theatre_name, l.row_id as row, l.seat_id as seat, l.section, name, s.sid From Locations l Left join Shows s on l.sid = s.sid) temp2 on temp1.location_id = temp2.location_id Where order_number is Null".format(
-                from_date=from_date, to_date=to_date))
+            "Select name as Show, row, seat, section, theatre_name as Theatre, cdate as Date, ctime as Time, price,  temp1.location_id, temp2.sid, temp1.ccid From (AvailableOn a inner join (Select cid as ccid, cdate, ctime From Calendars c Where c.cdate between CAST(%s AS DATE) and CAST(%s AS DATE)) cal on a.cid = cal.ccid) temp1 Left join (Select location_id, theatre_name, l.row_id as row, l.seat_id as seat, l.section, name, s.sid From Locations l Left join Shows s on l.sid = s.sid) temp2 on temp1.location_id = temp2.location_id Where order_number is Null",
+                from_date, to_date)
         for entry in cursor:
-            print(entry)
+            # print(entry)
 
             show = dict()
             show['name'] = entry[0]
@@ -86,7 +88,6 @@ def submitAvailabilityRequestByDate():
         cursor.close()
         context = dict(shows=shows)
         return render_template("find-tickets-process/availability.html", **context)
-        # return f'{from_date} should be earlier than {to_date}'
 
 
 @app.route('/fillPaymentInfo', methods=['POST'])
@@ -118,51 +119,73 @@ def completeOrder():
     c_info_dict['phone'] = c_phone
     c_info.append(c_info_dict)
 
-    print(chkbox_values)
-    print(c_name)
-    print(c_email)
-    print(c_phone)
-    print(c_date)
-    print(c_time)
-    print(c_discount)
-    if c_payment != []:
-        print(c_payment[0])
+    # print(chkbox_values)
+    # print(c_name)
+    # print(c_email)
+    # print(c_phone)
+    # print(c_date)
+    # print(c_time)
+    # print(c_discount)
+    # if c_payment != []:
+    #     print(c_payment[0])
 
     # get customer id (identify by email + phone) -> create an order --> get order id --> create an suborder
-    cursor = g.conn.execute("SELECT customerId FROM customers WHERE email ='{c_email}' and phone ='{c_phone}'".format(c_email=c_email, c_phone=c_phone))
+    # cursor = g.conn.execute("SELECT customerId FROM customers WHERE email ='{c_email}' and phone ='{c_phone}'".format(c_email=c_email, c_phone=c_phone))
+    cursor = g.conn.execute(
+        "SELECT customerId FROM customers WHERE email =%s and phone =%s", c_email,c_phone)
     if cursor.rowcount == 0:
-        g.conn.execute("INSERT INTO customers(name, email, phone) VALUES ('{c_name}', '{c_email}', '{c_phone}')".format(c_name=c_name, c_email=c_email, c_phone=c_phone))
+        # g.conn.execute("INSERT INTO customers(name, email, phone) VALUES ('{c_name}', '{c_email}', '{c_phone}')".format(c_name=c_name, c_email=c_email, c_phone=c_phone))
+        g.conn.execute("INSERT INTO customers(name, email, phone) VALUES (%s, %s, %s)",
+            c_name, c_email, c_phone)
     cursorN = g.conn.execute("SELECT * FROM customers")
 
     customerId = -1
     for entry in cursorN:
         customerId = entry[0]
 
-    print(customerId)
+    # print(customerId)
     if c_payment != [] and c_discount != '':
-        g.conn.execute("INSERT INTO Orders(customerId, discount_code, order_date, order_time, payment_method) VALUES ('{customerId}', '{discount_code}', '{order_date}', '{order_time}', '{payment_method}')".format(customerId=customerId,discount_code=c_discount,order_date=c_date, order_time=c_time, payment_method = c_payment[0]))
+        # g.conn.execute("INSERT INTO Orders(customerId, discount_code, order_date, order_time, payment_method) VALUES ('{customerId}', '{discount_code}', '{order_date}', '{order_time}', '{payment_method}')".format(customerId=customerId,discount_code=c_discount,order_date=c_date, order_time=c_time, payment_method = c_payment[0]))
+        g.conn.execute(
+            "INSERT INTO Orders(customerId, discount_code, order_date, order_time, payment_method) VALUES (%s, %s, %s, %s, %s)",
+                customerId, c_discount, c_date, c_time,c_payment[0])
     elif c_payment != [] and c_discount == '':
-        g.conn.execute("INSERT INTO Orders(customerId, order_date, order_time, payment_method) VALUES ('{customerId}', '{order_date}', '{order_time}', '{payment_method}')".format(customerId=customerId,order_date=c_date, order_time=c_time, payment_method = c_payment[0]))
+        g.conn.execute(
+            "INSERT INTO Orders(customerId, order_date, order_time, payment_method) VALUES (%s, %s, %s, %s)",
+            customerId, c_date, c_time, c_payment[0])
+        # g.conn.execute("INSERT INTO Orders(customerId, order_date, order_time, payment_method) VALUES ('{customerId}', '{order_date}', '{order_time}', '{payment_method}')".format(customerId=customerId,order_date=c_date, order_time=c_time, payment_method = c_payment[0]))
     elif c_payment == [] and c_discount != '':
-        g.conn.execute("INSERT INTO Orders(customerId, discount_code, order_date, order_time) VALUES ('{customerId}', '{discount_code}', '{order_date}', '{order_time}')".format(customerId=customerId, discount_code=c_discount, order_date=c_date, order_time=c_time))
+        g.conn.execute(
+            "INSERT INTO Orders(customerId, discount_code, order_date, order_time) VALUES (%s, %s, %s, %s)",
+            customerId, c_discount, c_date, c_time)
+        # g.conn.execute("INSERT INTO Orders(customerId, discount_code, order_date, order_time) VALUES ('{customerId}', '{discount_code}', '{order_date}', '{order_time}')".format(customerId=customerId, discount_code=c_discount, order_date=c_date, order_time=c_time))
     else:
-        g.conn.execute("INSERT INTO Orders(customerId, order_date, order_time) VALUES ('{customerId}', '{order_date}', '{order_time}')".format(customerId=customerId, order_date=c_date, order_time=c_time))
+        g.conn.execute(
+            "INSERT INTO Orders(customerId, order_date, order_time) VALUES (%s, %s, %s)",
+            customerId, c_date, c_time)
+        # g.conn.execute("INSERT INTO Orders(customerId, order_date, order_time) VALUES ('{customerId}', '{order_date}', '{order_time}')".format(customerId=customerId, order_date=c_date, order_time=c_time))
 
-    cursorO = g.conn.execute("SELECT order_number FROM Orders WHERE customerId ='{customerId}' and order_date ='{order_date}' and order_time = '{order_time}'".format(customerId=customerId, order_date=c_date, order_time=c_time))
+    # cursorO = g.conn.execute("SELECT order_number FROM Orders WHERE customerId ='{customerId}' and order_date ='{order_date}' and order_time = '{order_time}'".format(customerId=customerId, order_date=c_date, order_time=c_time))
+    cursorO = g.conn.execute(
+        "SELECT order_number FROM Orders WHERE customerId =%s and order_date =%s and order_time =%s",
+            customerId, c_date, c_time)
     order_number = -1
     for entry in cursorO:
         order_number = entry[0]
-    print(order_number)
+    # print(order_number)
 
     for v in chkbox_values:
         v_list = [int(item) if item.isdigit() else item for item in v.split(',')]
         cid = v_list[2]
         location_id = v_list[0]
-        g.conn.execute("INSERT INTO SubOrders(order_number, cid, location_id) VALUES ('{order_number}', '{cid}', '{location_id}')".format(order_number=order_number, cid=cid, location_id=location_id))
+        # g.conn.execute("INSERT INTO SubOrders(order_number, cid, location_id) VALUES ('{order_number}', '{cid}', '{location_id}')".format(order_number=order_number, cid=cid, location_id=location_id))
+        g.conn.execute(
+            "INSERT INTO SubOrders(order_number, cid, location_id) VALUES (%s, %s, %s)",
+                order_number, cid, location_id)
 
-    cursorTest = g.conn.execute("SELECT * FROM SubOrders")
-    for entry in cursorTest:
-        print(entry[0])
+    # cursorTest = g.conn.execute("SELECT * FROM SubOrders")
+    # for entry in cursorTest:
+    #     print(entry[0])
 
     c_info_dict['order_id'] = order_number
     context = dict(customers=c_info)
@@ -192,8 +215,10 @@ def prevOrders():
 @app.route('/findByOrderNumber', methods=['POST'])
 def findByOrderNumber():
     order_number = request.form.get('ByOrderNumber')
+    # cursor = g.conn.execute(
+    #     f"SELECT order_date, order_time, row_id, seat_id, theatre_name, section, cdate, ctime, name, Onum FROM (((SELECT *, o.order_number as Onum FROM orders o LEFT JOIN SubOrders so on o.order_number = so.order_number WHERE o.order_number = {order_number}) AS temp1 INNER JOIN Locations l on temp1.location_id = l.location_id) temp2 INNER JOIN Calendars c on temp2.cid = c.cid) temp3 INNER JOIN Shows s on temp3.sid = s.sid")
     cursor = g.conn.execute(
-        f"SELECT order_date, order_time, row_id, seat_id, theatre_name, section, cdate, ctime, name, Onum FROM (((SELECT *, o.order_number as Onum FROM orders o LEFT JOIN SubOrders so on o.order_number = so.order_number WHERE o.order_number = {order_number}) AS temp1 INNER JOIN Locations l on temp1.location_id = l.location_id) temp2 INNER JOIN Calendars c on temp2.cid = c.cid) temp3 INNER JOIN Shows s on temp3.sid = s.sid")
+            "SELECT order_date, order_time, row_id, seat_id, theatre_name, section, cdate, ctime, name, Onum FROM (((SELECT *, o.order_number as Onum FROM orders o LEFT JOIN SubOrders so on o.order_number = so.order_number WHERE o.order_number =%s) AS temp1 INNER JOIN Locations l on temp1.location_id = l.location_id) temp2 INNER JOIN Calendars c on temp2.cid = c.cid) temp3 INNER JOIN Shows s on temp3.sid = s.sid", order_number)
     orders = []
     for entry in cursor:
         order = dict()
@@ -216,8 +241,10 @@ def findByOrderNumber():
 @app.route('/findByEmail', methods=['POST'])
 def findByByEmail():
     email = request.form.get('ByEmail')
+    # cursor = g.conn.execute(
+    #     f"SELECT order_date, order_time, row_id, seat_id, theatre_name, section, cdate, ctime, s.name, Onum FROM (SELECT *, Orders.order_number as Onum FROM Orders LEFT JOIN SubOrders on Orders.order_number = SubOrders.order_number) AS TEMP1 INNER JOIN (SELECT * FROM Customers WHERE Customers.email = '{email}') Cust on TEMP1.customerid = Cust.customerid INNER JOIN Locations l on TEMP1.location_id = l.location_id INNER JOIN Calendars c on TEMP1.cid = c.cid LEFT JOIN Shows s on l.sid = s.sid")
     cursor = g.conn.execute(
-        f"SELECT order_date, order_time, row_id, seat_id, theatre_name, section, cdate, ctime, s.name, Onum FROM (SELECT *, Orders.order_number as Onum FROM Orders LEFT JOIN SubOrders on Orders.order_number = SubOrders.order_number) AS TEMP1 INNER JOIN (SELECT * FROM Customers WHERE Customers.email = '{email}') Cust on TEMP1.customerid = Cust.customerid INNER JOIN Locations l on TEMP1.location_id = l.location_id INNER JOIN Calendars c on TEMP1.cid = c.cid LEFT JOIN Shows s on l.sid = s.sid")
+        "SELECT order_date, order_time, row_id, seat_id, theatre_name, section, cdate, ctime, s.name, Onum FROM (SELECT *, Orders.order_number as Onum FROM Orders LEFT JOIN SubOrders on Orders.order_number = SubOrders.order_number) AS TEMP1 INNER JOIN (SELECT * FROM Customers WHERE Customers.email =%s) Cust on TEMP1.customerid = Cust.customerid INNER JOIN Locations l on TEMP1.location_id = l.location_id INNER JOIN Calendars c on TEMP1.cid = c.cid LEFT JOIN Shows s on l.sid = s.sid", email)
     orders = []
     for entry in cursor:
         order = dict()
